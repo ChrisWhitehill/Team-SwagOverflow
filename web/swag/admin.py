3160bfc19d3c195ad87c10e6336f76b0108d9285
Brobin <tobinjbrown@gmail.com>
2016-02-27 09:18:03 -0600

    make the admin easier to use

--- web/swag/admin.py
+++ web/swag/admin.py
@@ -6,10 +6,36 @@ class UserAdmin(admin.ModelAdmin):
     list_display_links = ['id', 'name', 'email']
 
 
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'league']
+    list_display_links = ['id', 'name']
+
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+
+class TeamEventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'away_team', 'home_team', 'date', 'channel_name', 'channel_number']
+
+
+class ShowEventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'show', 'date', 'channel_name', 'channel_number']
+
+
+class FavoriteTeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'team', 'user']
+
+
+class FavoriteShowAdmin(admin.ModelAdmin):
+    list_display = ['id', 'show', 'user']
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Team)
-admin.site.register(Show)
-admin.site.register(TeamEvent)
-admin.site.register(ShowEvent)
-admin.site.register(FavoriteTeam)
-admin.site.register(FavoriteShow)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Show, ShowAdmin)
+admin.site.register(TeamEvent, TeamEventAdmin)
+admin.site.register(ShowEvent, ShowEventAdmin)
+admin.site.register(FavoriteTeam, FavoriteTeamAdmin)
+admin.site.register(FavoriteShow, FavoriteShowAdmin)
