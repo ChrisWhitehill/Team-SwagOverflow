d807f37395e686132fa000628bf79ffc0d9ebc17
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2016-02-27 09:53:21 -0600

    preference cell polish

--- iOS/CSGiOSClient/CSGiOSClient/PreferenceTableViewCell.swift
+++ iOS/CSGiOSClient/CSGiOSClient/PreferenceTableViewCell.swift
@@ -13,11 +13,15 @@ class PreferenceTableViewCell: UITableViewCell {
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     
+    var team: Team?
+    var show: Show?
+    
     @IBAction func deleteTapped() {
         print("delete")
     }
     
     func displayForTeam(team: Team) {
+        self.team = team
         nameLabel.text = team.name
         
         if let logo = team.logo_url, let url  = NSURL(string: logo), data = NSData(contentsOfURL: url) {
@@ -26,6 +30,7 @@ class PreferenceTableViewCell: UITableViewCell {
     }
     
     func displayForShow(show: Show) {
+        self.show = show
         nameLabel.text = show.name
         
         if let logo = show.logo_url, let url  = NSURL(string: logo), data = NSData(contentsOfURL: url) {
