17b1400e2c59c67c9b838dd2f49a774d96599b6d
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2016-02-27 07:21:25 -0600

    base project

---
+++ iOS/CSGiOSClient/CSGiOSClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSGiOSClient
+//
+//  Created by Jacob Sanchez on 2/27/16.
+//  Copyright © 2016 jacob.sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
